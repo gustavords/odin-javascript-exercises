08b3bab53eb75a0d998701661903894c1c2488c2
gustavords <gustavords92@gmail.com>
2023-04-21 02:41:16 -0400

    Pass npm test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+    let reverseStr = ``;
+    let n = str.length - 1 ;
+    do{
+        reverseStr += str.charAt(n);
+        n--;
+    }while(n >= 0);
+    
+    return reverseStr;
 };
 
 // Do not edit below this line
