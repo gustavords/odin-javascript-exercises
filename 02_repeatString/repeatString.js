b3a35421b0598c70de0fbe15ded3fd38e3b3cc57
gustavords <gustavords92@gmail.com>
2023-04-21 02:01:41 -0400

    Pass npm test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function (str, n) {
+    let concatStr = ``;
+    while (n > 0) {
+        concatStr += str;
+        n--;
+    }
+    
+    return (n < 0) ? `ERROR` : concatStr;
 };
 
 // Do not edit below this line
