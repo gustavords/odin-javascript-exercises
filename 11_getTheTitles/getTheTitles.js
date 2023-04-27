79a9f3e897f9391f81b0575872034c8ad99a7d64
gustavords <gustavords92@gmail.com>
2023-04-27 15:13:21 -0400

    Pass npm test

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,25 @@
-const getTheTitles = function() {
+const getTheTitles = function (books) {
+    const arr = [];
+    books.forEach(x => {
+        arr.push(x.title);
+        console.log(arr)
+    });
+    return arr;
 
+    // also
+    // const arr2 = [];
+    // for (let book of books) {
+    //     if (`title` in book) {
+    //             console.log(`this is the title: ` + book[`title`]);
+    //             arr2.push(book[`title`]);
+    //         }
+    // }
+    // return arr2;
+
+    //both work
+
+    //also
+   
 };
 
 // Do not edit below this line
