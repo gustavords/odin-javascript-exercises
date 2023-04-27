676dddd9d1238b1892309935c55e97e260f11db5
gustavords <gustavords92@gmail.com>
2023-04-26 22:42:33 -0400

    Pass npm test

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+    let i = 1;
+    let arr = [0, 1];
+    if(n < 0) return `OOPS`;
+    if(n<=0) return 1;
 
+    for(;n >= arr[1]; n--){
+        arr.push(arr[i]+arr[i-1]);
+        console.log(n)
+        i++;
+    }
+    console.log(arr);
+    console.log(arr[arr.length - 2]);
+    return arr[arr.length - 2];
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
