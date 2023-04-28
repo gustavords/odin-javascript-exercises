150e7a2884e0c0d9ae497cfdbcf77065698b48a4
gustavords <gustavords92@gmail.com>
2023-04-28 14:52:10 -0400

    Pass npm test

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -7,27 +7,52 @@ const findTheOldest = function (array) {
     //if older place into oldest variable
     //returns object
     
+    // let currentYear = new Date().getFullYear()
+    // let oldest = 0;
+    // let oldestPerson = {};
+
+    // for (let object of array) {
+    //     let age = object.yearOfDeath - object.yearOfBirth;
+    //     if (object.yearOfDeath === undefined) {
+    //         age = currentYear - object.yearOfBirth;
+    //         console.log(`this bitch is alive:` + age);
+    //     }
+    //     console.log(age);
+    //     if (age > oldest) {
+    //         oldest = age;
+    //         for (let key in object) {
+    //             oldestPerson[key] = object[key];
+    //         }
+    //         console.log(`new oldest: ` + age);
+    //     }
+    // }
+
+    // return oldestPerson;
+
+
+    //also works
+
     let currentYear = new Date().getFullYear()
-    let oldest = 0;
-    let oldestPerson = {};
+    
+    let map = array.map((key) => {
+        if(!(`yearOfDeath`in key)){
+             key.yearOfDeath = currentYear;
+        } 
+        key.age = key.yearOfDeath - key.yearOfBirth;
+        return key;
+    });
+    
+    // console.log(map);
+    
+    let reduce = map.reduce((comparison, current) =>{
+        if(comparison.age < current.age) comparison = current;
+        return comparison;
+    });
+    
+    return reduce;
 
-    for (let object of array) {
-        let age = object.yearOfDeath - object.yearOfBirth;
-        if (object.yearOfDeath === undefined) {
-            age = currentYear - object.yearOfBirth;
-            console.log(`this bitch is alive:` + age);
-        }
-        console.log(age);
-        if (age > oldest) {
-            oldest = age;
-            for (let key in object) {
-                oldestPerson[key] = object[key];
-            }
-            console.log(`new oldest: ` + age);
-        }
-    }
 
-    return oldestPerson;
+
 
 };
 
